812cfb26d1f9e59003bd838a8d969c6855145abe
dino <cpollano@hotmail.co.uk>
2013-11-19 12:11:13 +0000

    stopped working

--- midiHoot/OWL2ButtonMidi/OWL2ButtonMidi.ino
+++ midiHoot/OWL2ButtonMidi/OWL2ButtonMidi.ino
@@ -4,11 +4,10 @@
 // ISR interrupt service routine
 #include < avr/interrupt.h >
 
-const int button1Pin = 2;// analog pin that button 1 is connect to 
-const int button2Pin = 3; // analog pin that button 2 is connect to 
+const int button1Pin = 2;// analog pin that button 1 is connect to
+const int button2Pin = 3; // analog pin that button 2 is connect to
 const int analogOutPin = 8 ;  // analog pin that Led 1 is connected to
 
-     
 int buttonState1 = 0;         // variable for reading the pushbutton status
 int buttonState2 = 0;         // variable for reading the pushbutton status
 int lastButtonState1 = LOW;   // the previous reading from the input pin
@@ -19,37 +18,29 @@ volatile int reading2 = LOW;
 
 
 
-void setup() 
+void setup()
 {
   // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
-  
-  
+  Serial.begin(9600);
   pinMode(button1Pin, INPUT);  // set the button1 as an input to read
   digitalWrite(button1Pin, HIGH);    // Enable pullup resistor
   pinMode(button2Pin, INPUT);
   pinMode(analogOutPin,   OUTPUT);  // set the button1 as an input to read
   sei();                    // Enable global interrupts
   EIMSK |= (1 << INT0);     // Enable external interrupt INT0
-  
   EICRA |= (1 << ISC01);    // Trigger INT0 on falling edge
 
 }
 
-void loop() 
+void loop()
 {
-  
-  
-
 //reading1 = digitalRead(button1Pin);
-reading2 = digitalRead(button2Pin);
-
+  reading2 = digitalRead(button2Pin);
 //buttonState1 = reading1;
-buttonState2 = reading2; 
+  buttonState2 = reading2;
  
 
     // if the button state has changed:
-   
     if (buttonState1 != lastButtonState1) 
     {
       if(buttonState1 == HIGH)
@@ -80,12 +71,10 @@ buttonState2 = reading2;
        digitalWrite(analogOutPin, LOW);
       }
       lastButtonState2 = buttonState2;
-    }
- 
-                     
+    }                
 }
 
 ISR(INT0_vect)
 {
-  Serial.print("interrupt 1 works");
+  Serial.print("interrupt 1 works \n");
 }
