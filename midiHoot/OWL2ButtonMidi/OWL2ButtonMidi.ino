f4803562f0e405fdb975c0e5a428ea26189cf112
dino <cpollano@hotmail.co.uk>
2014-07-15 01:29:20 +0100

    added LED functions, need to add retrieval method and way to enter midi assign mode

--- midiHoot/OWL2ButtonMidi/OWL2ButtonMidi.ino
+++ midiHoot/OWL2ButtonMidi/OWL2ButtonMidi.ino
@@ -16,6 +16,12 @@
 #define PATCH_PARAMETER_C 22
 #define PATCH_PARAMETER_D 23
 #define PATCH_PARAMETER_E 24
+#define ledPin1  8
+#define ledPin2  9 
+#define LED_ON 0 
+#define LED_SLOW 1 
+#define LED_FAST 2
+
 
 bool midiAssignMode = false; 
 
@@ -24,11 +30,11 @@ byte storedValues[5] = {0,0,0,0,0};
 byte pedalValues[5] = {0,0,0,0,0}; 
 const int button1Pin = 2;// analog pin that button 1 is connect to, button 1 is toggle 
 const int button2Pin = 3; // analog pin that button 2 is connect to, button 2 is momentary 
-const int ledPin1 =  8;  
+/*const int ledPin1 =  8;  
 int led1State = LOW;
 const int ledPin2 =  9;  
 int led2State = LOW;
-
+*/
 
 int buttonState2 = 0;         // variable for reading the pushbutton status
 int lastButtonState2 = LOW;   // the previous reading from the input pin
@@ -55,8 +61,10 @@ void setup()
   pinMode(button2Pin, INPUT);
   digitalWrite(button1Pin, HIGH);    // Enable pullup resistor
   digitalWrite(button2Pin, HIGH);    // Enable pullup resistor
-  pinMode(ledPin1,   OUTPUT);  // set the button1 as an input to read
-  pinMode(ledPin2,   OUTPUT);  // set the button1 as an input to read
+  pinMode(ledPin1, OUTPUT);  
+  pinMode(ledPin2,   OUTPUT);  
+   digitalWrite(ledPin1, LOW);    
+  digitalWrite(ledPin2, LOW);  
   
   sei();                    // Enable global interrupts
   EIMSK |= (1 << INT0);     // Enable external interrupt INT0
@@ -88,7 +96,7 @@ void HandleControlChange(byte channel, byte number, byte value)
 {
   if(midiAssignMode)
  {
-   switch(Number)
+   switch(number)
    {
      case PATCH_PARAMETER_A:
      {
@@ -125,7 +133,7 @@ void HandleControlChange(byte channel, byte number, byte value)
  else
  {
    
-    switch(Number)
+    switch(number)
    {
      case PATCH_PARAMETER_A:
      {
@@ -235,9 +243,130 @@ void HandleNoteOn(byte channel, byte pitch, byte velocity) // note on
 
 /*****************Midi in************/ 
 
+
+
+
+
+/**********************************main*************************/
+void loop()
+{
+
+ MIDI.read();
+
+if(midiAssignMode == true)
+{
+  LED_FUNCTION(ledPin1,LED_SLOW);
+  LED_FUNCTION(ledPin2,LED_SLOW);
+}
+
+  
+  if(button1ChangeFlag != false)
+  { 
+     
+      lastDebounceTime1 = millis(); 
+      button1ChangeFlag = false;
+  }
+ 
+
+   if(button2ChangeFlag != false)
+  {
+         
+     lastDebounceTime2 = millis(); 
+     
+     button2ChangeFlag = false;
+  }
+  
+  
+                     /****DEBOUNCING***/
+  if((lastDebounceTime1 != 0) && (millis()-lastDebounceTime1) > debounceDelay1)  
+   {
+     
+     if (lastButton1Toggle == true)
+     {
+       button1Toggle = false;
+      // Serial.print("debouncing stage - button off \n");
+     }
+     if (lastButton1Toggle == false)
+     {
+      button1Toggle = true;
+       // Serial.print("debouncing stage - button on \n");
+     }
+      lastDebounceTime1 = 0;
+      button1ChangeFlag = false; 
+    
+   }
+   
+   
+   if((lastDebounceTime2 != 0) && (millis()-lastDebounceTime2) > debounceDelay2)
+   {
+      midiAssignMode = false;
+     buttonState2 = digitalRead(button2Pin); 
+     lastDebounceTime2 = 0;
+     button2ChangeFlag = false; 
+
+   }
+
+                               /**OUTPUT**/
+ if(midiAssignMode != false)
+ {
+    // if the button state has changed:
+    if ( button1Toggle != lastButton1Toggle )   
+    {
+      
+
+      if(  button1Toggle == true )
+      {
+        //led1State = HIGH;
+                LED_FUNCTION(ledPin1, LED_ON); //on
+        MIDI.sendNoteOn(C3, 127, 1); 
+       // Serial.print(" output stage - button on \n"); 
+      }
+     else if(button1Toggle == false)
+      {
+      // led1State = LOW;
+        LED_FUNCTION(ledPin1, LED_ON); //off
+        MIDI.sendNoteOff(C3, 0, 1); 
+        //Serial.print(" Output stage - button off \n");
+      }
+
+    
+       // digitalWrite(ledPin1,led1State); 
+       lastButton1Toggle = button1Toggle;
+      
+    }
+    
+    
+       if (buttonState2 != lastButtonState2 || midiTrigger2 != lastMidiTrigger2 ) 
+    {   
+       
+  
+       if(buttonState2 == LOW || midiTrigger2 == true) 
+      {
+      //led2State = HIGH;
+        LED_FUNCTION(ledPin2,LED_ON); // On
+        MIDI.sendNoteOn(A3, 127, 1);
+        
+                 
+      }
+      else if(buttonState2 == HIGH && midiTrigger2 == false) 
+      {
+     
+        //led2State = LOW;
+         MIDI.sendNoteOff(A3, 0, 1); 
+         LED_FUNCTION(ledPin2,LED_ON); // Off 
+      }
+      // digitalWrite(ledPin2,led2State); 
+      lastButtonState2 = buttonState2;
+      lastMidiTrigger2 = midiTrigger2; 
+    }   
+ }    
+}
+
+/************main********/ 
+
 void SENDCC(byte Values[5])
 {
-  for(int i = 0, => 5)
+  for(int i = 0; i >= 4;)
  {
     switch(i)
     {
@@ -275,109 +404,60 @@ void SENDCC(byte Values[5])
  } 
 }
 
-
-
-/**********************************main*************************/
-void loop()
+void LED_FUNCTION(int LED, int mode)
 {
-
- MIDI.read();
-
   
-  if(button1ChangeFlag != false)
-  { 
-       
-      lastDebounceTime1 = millis(); 
-      button1ChangeFlag = false;
-  }
- 
-
-   if(button2ChangeFlag != false)
+  switch(mode)
   {
-         
-     lastDebounceTime2 = millis(); 
-     
-     button2ChangeFlag = false;
-  }
-  
-  
-                     /****DEBOUNCING***/
-  if((lastDebounceTime1 != 0) && (millis()-lastDebounceTime1) > debounceDelay1)  
-   {
-     
-     if (lastButton1Toggle == true)
-     {
-       button1Toggle = false;
-      // Serial.print("debouncing stage - button off \n");
-     }
-     if (lastButton1Toggle == false)
-     {
-      button1Toggle = true;
-       // Serial.print("debouncing stage - button on \n");
-     }
-      lastDebounceTime1 = 0;
-      button1ChangeFlag = false; 
-    
-   }
-   
-   
-   if((lastDebounceTime2 != 0) && (millis()-lastDebounceTime2) > debounceDelay2)
-   {
-     buttonState2 = digitalRead(button2Pin); 
-     lastDebounceTime2 = 0;
-     button2ChangeFlag = false; 
-
-   }
-
-                               /**OUTPUT**/
-
-    // if the button state has changed:
-    if ( button1Toggle != lastButton1Toggle )   
+    case LED_ON:
     {
       
-
-      if(  button1Toggle == true )
+      if( !digitalRead(LED) )
       {
-        led1State = HIGH;
-        MIDI.sendNoteOn(C3, 127, 1); 
-       // Serial.print(" output stage - button on \n"); 
+        digitalWrite(LED, HIGH);
       }
-     else if(button1Toggle == false)
+      else if( digitalRead(LED))
       {
-       led1State = LOW;
-        MIDI.sendNoteOff(C3, 0, 1); 
-        //Serial.print(" Output stage - button off \n");
+        digitalWrite(LED, LOW); 
       }
-
-    
-        digitalWrite(ledPin1,led1State); 
-       lastButton1Toggle = button1Toggle;
-      
+      break; 
     }
     
+    case LED_SLOW:
+    {
+      digitalWrite(LED,HIGH);
+      delay(500); 
+      digitalWrite(LED,LOW);
+      delay(500);
+       digitalWrite(LED,HIGH);
+      delay(500); 
+      digitalWrite(LED,LOW);
+      delay(500);
+      digitalWrite(LED,HIGH);
+      delay(500); 
+      digitalWrite(LED,LOW);
+      break; 
+    }
+     case LED_FAST:
+    {
+      digitalWrite(LED,HIGH);
+      delay(250); 
+      digitalWrite(LED,LOW);
+      delay(250);
+       digitalWrite(LED,HIGH);
+      delay(250); 
+      digitalWrite(LED,LOW);
+      delay(250);
+      digitalWrite(LED,HIGH);
+      delay(250); 
+      digitalWrite(LED,LOW);
+      break; 
+    }
     
-       if (buttonState2 != lastButtonState2 || midiTrigger2 != lastMidiTrigger2 ) 
-    {   
-       
+    default:
+    {
+      break; 
+    }
+  }
   
-       if(buttonState2 == LOW || midiTrigger2 == true) 
-      {
-      led2State = HIGH;
-       
-        MIDI.sendNoteOn(A3, 127, 1);
-        
-                 
-      }
-      else if(buttonState2 == HIGH && midiTrigger2 == false) 
-      {
-     
-        led2State = LOW;
-         MIDI.sendNoteOff(A3, 0, 1); 
-      }
-       digitalWrite(ledPin2,led2State); 
-      lastButtonState2 = buttonState2;
-      lastMidiTrigger2 = midiTrigger2; 
-    }               
 }
-
-/************main********/ 
